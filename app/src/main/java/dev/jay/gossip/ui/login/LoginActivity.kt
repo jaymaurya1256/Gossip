e33afa0ff8c7757f539c13349b3a9ad01a20fa27
jay <jaymaurya1256@gmail.com>
2022-12-25 22:03:16 +0530

    Implement Authentication

--- app/src/main/java/dev/jay/gossip/ui/login/LoginActivity.kt
+++ app/src/main/java/dev/jay/gossip/ui/login/LoginActivity.kt
@@ -1,16 +1,45 @@
 package dev.jay.gossip.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.navigation.findNavController
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 import dev.jay.gossip.R
 import dev.jay.gossip.databinding.ActivityLoginBinding
+import dev.jay.gossip.ui.main.MainActivity
+import dev.jay.gossip.ui.signup.SignupActivity
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityLoginBinding.inflate(layoutInflater)
+        auth = FirebaseAuth.getInstance()
         setContentView(binding.root)
 
+        binding.loginButton.setOnClickListener{
+            val email = binding.email.text.toString()
+            val password = binding.password.text.toString()
+            if (email.isEmpty() || password.isEmpty()){
+                Snackbar.make(this,binding.root,"Either email or password is empty", Snackbar.LENGTH_SHORT).show()
+            }else{
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if (it.isSuccessful){
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    }else{
+                        Snackbar.make(this,binding.root,"Invalid Email or Password", Snackbar.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
 
+        binding.goToSignup.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/dev/jay/gossip/ui/signup/SignupActivity.kt
+++ app/src/main/java/dev/jay/gossip/ui/signup/SignupActivity.kt
@@ -1,12 +1,42 @@
 package dev.jay.gossip.ui.signup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 import dev.jay.gossip.R
+import dev.jay.gossip.databinding.ActivitySignupBinding
+import dev.jay.gossip.ui.login.LoginActivity
 
 class SignupActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        val binding = ActivitySignupBinding.inflate(layoutInflater)
+        auth = FirebaseAuth.getInstance()
+        setContentView(binding.root)
+
+        binding.signup.setOnClickListener {
+            val email = binding.email.text.toString()
+            val password = binding.password.text.toString()
+            if (email.isEmpty() || password.isEmpty()){
+                Snackbar.make(this,binding.root,"Either email or password is empty", Snackbar.LENGTH_SHORT).show()
+            }else{
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if (it.isSuccessful){
+                        Snackbar.make(this,binding.root,"User is successfully Created", Snackbar.LENGTH_SHORT).show()
+                    }else{
+                        Snackbar.make(this,binding.root,"User not created", Snackbar.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+
+        binding.backToLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }

c33826603ce2bffa5901de1090f8a69ec9cb7474
jay <jaymaurya1256@gmail.com>
2023-01-09 13:39:40 +0530

    Handled the Activity crashes at the start of MainActivity

--- app/src/main/java/dev/jay/gossip/ui/main/activity/MainActivity.kt
+++ app/src/main/java/dev/jay/gossip/ui/main/activity/MainActivity.kt
@@ -4,25 +4,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
-import com.google.android.material.snackbar.Snackbar
-import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
 import dev.jay.gossip.databinding.ActivityMainBinding
+import dev.jay.gossip.ui.signup.SignupActivity
 import kotlin.system.exitProcess
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val viewModel : MainViewModel by viewModels()
-    private lateinit var auth: FirebaseAuth
+//    private val viewModel : MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        auth = FirebaseAuth.getInstance()
-        if (auth.currentUser == null) {
-            val intent = Intent(this, MainActivity::class.java)
+        if (Firebase.auth.currentUser == null) {
+            val intent = Intent(this, SignupActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
             startActivity(intent)
         }

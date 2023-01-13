db605365819f8609338b5833bb3f86ca44486f83
jay <jaymaurya1256@gmail.com>
2023-01-13 16:30:33 +0530

    Fix Navigation Drawer header UI

--- app/src/main/java/dev/jay/gossip/ui/main/activity/MainActivity.kt
+++ app/src/main/java/dev/jay/gossip/ui/main/activity/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.get
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.ktx.auth
--- app/src/main/java/dev/jay/gossip/ui/main/activity/MainViewModel.kt
+++ app/src/main/java/dev/jay/gossip/ui/main/activity/MainViewModel.kt
@@ -1,19 +1,11 @@
 package dev.jay.gossip.ui.main.activity
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.google.firebase.auth.FirebaseUser
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dev.jay.gossip.database.User
-import dev.jay.gossip.database.UserDAO
-import dev.jay.gossip.database.UserDatabase
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val db: UserDAO
 ): ViewModel() {
 
 }

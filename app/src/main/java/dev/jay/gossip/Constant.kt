1636c5879f67f69b18dd4a1b3e84c56daaa7b080
jay <jaymaurya1256@gmail.com>
2023-06-19 13:52:17 +0530

    Add chipset for tags in add tags fragment

---
+++ app/src/main/java/dev/jay/gossip/Constant.kt
@@ -0,0 +1,5 @@
+package dev.jay.gossip
+
+object Constant {
+    val TAGS = arrayOf("Android", "Tech", "Sports", "Business", "Politics")
+}
--- app/src/main/java/dev/jay/gossip/ui/add/AddGossipFragment.kt
+++ app/src/main/java/dev/jay/gossip/ui/add/AddGossipFragment.kt
@@ -10,11 +10,13 @@ import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.chip.Chip
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.ktx.app
 import dagger.hilt.android.AndroidEntryPoint
+import dev.jay.gossip.Constant
 import dev.jay.gossip.R
 import dev.jay.gossip.databinding.FragmentAddGossipBinding
 import dev.jay.gossip.documents.Gossip
@@ -54,6 +56,24 @@ class AddGossipFragment : BottomSheetDialogFragment() {
                     )
                 } catch (e: Exception) {
                     Snackbar.make(binding.root, getString(R.string.something_went_wrong_gossip_not_added), Snackbar.LENGTH_SHORT).show()
+                }
+            }
+        }
+        binding.buttonAddTag.setOnClickListener {
+            if (binding.addTagChipGroup.visibility == View.VISIBLE) {
+                binding.addTagChipGroup.visibility = View.GONE
+                binding.buttonAddTag.text = getString(R.string.add_tag_or_keyword)
+            }
+            else{
+                binding.addTagChipGroup.visibility = View.VISIBLE
+                binding.buttonAddTag.text = getString(R.string.hide_tags)
+                binding.addTagChipGroup.removeAllViews()
+                for (tag in Constant.TAGS) {
+                    val chip = Chip(requireContext())
+                    chip.text = tag
+                    chip.isClickable = true
+                    chip.isCheckable = true
+                    binding.addTagChipGroup.addView(chip)
                 }
             }
         }
